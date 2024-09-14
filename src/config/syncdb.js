3905fb43204480262643333747890d62482679fb
Yogesh Dhakal <76492904+yogesdhakal@users.noreply.github.com>
2024-09-14 21:58:56 +0200

    Update syncdb.js

--- src/config/syncdb.js
+++ src/config/syncdb.js
@@ -3,14 +3,14 @@ const participants = require("../models/participant");
 
 const syncTablesToDb = async () => {
   try {
-    await admins.truncate();
+    
     await admins.sync({ alter: true });
     console.log("The admins table has been synced.");
   } catch (err) {
     console.error("Unable to sync the admins table:", err);
   }
   try {
-    await participants.truncate();
+    
     await participants.sync({ alter: true });
     console.log("The participants table has been synced.");
   } catch (err) {
